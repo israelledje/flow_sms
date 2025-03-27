efa35f0fb6d35694dcab01751c11460f010038db
PC <israelledje@gmail.com>
2025-03-27 15:00:13 +0100

    configs pour heberger sur render

--- flow_sms/urls.py
+++ flow_sms/urls.py
@@ -19,6 +19,8 @@ from django.urls import path, include
 from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,3 +28,5 @@ urlpatterns = [
     path('campaigns/', include('campaigns.urls')),
     path('', RedirectView.as_view(url='user/login/', permanent=False), name='index'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
